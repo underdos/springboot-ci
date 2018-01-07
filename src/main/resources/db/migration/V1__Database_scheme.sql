57c49c0f834abf6bbf191561802b29b0b824cec0
Kusnadi <kus.underdos@gmail.com>
2018-01-07 13:58:09 +0700

    minimize app

--- src/main/resources/db/migration/V1__Database_scheme.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE User(
-    id int8 NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (email)
-)
--- src/main/resources/db/migration/V2__Init_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO User VALUES (1, "Kusnadi", "kus.underdos@gmail.com");
-INSERT INTO User VALUES (2, "Kunay", "kusnadi@yahoo.com");
-INSERT INTO User VALUES (3, "Encus", "kusnadiworks@gmail.com");
