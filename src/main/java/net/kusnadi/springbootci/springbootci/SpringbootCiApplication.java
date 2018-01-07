c05ad324bded21b6d16b06a151f3ba42849569f2
Kusnadi <kus.underdos@gmail.com>
2018-01-07 11:42:00 +0700

    First Commit

---
+++ src/main/java/net/kusnadi/springbootci/springbootci/SpringbootCiApplication.java
@@ -0,0 +1,12 @@
+package net.kusnadi.springbootci.springbootci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCiApplication.class, args);
+	}
+}
